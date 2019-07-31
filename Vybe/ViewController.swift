4aa9dad4314bc867211853ea3f8a88cf64fb50c5
Terry Bridges <trbg@_outlook.com>
2019-07-31 15:12:36 +0100

    Swipe gesture and animation

--- Vybe/ViewController.swift
+++ Vybe/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var thumbImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        if xFromCenter > 0 {
+            thumbImg.image = #imageLiteral(resourceName: "thumbsup")
+            thumbImg.tintColor = UIColor.green
+        } else {
+            thumbImg.image = #imageLiteral(resourceName: "thumbsdown")
+            thumbImg.tintColor = UIColor.red
+        }
+        
+        thumbImg.alpha = abs(xFromCenter) / view.center.x
+        
+        if sender.state == UIGestureRecognizer.State.ended {
+            UIView.animate(withDuration: 0.2) {
+                card.center = self.view.center
+                self.thumbImg.alpha = 0
+            }
+        }
+    }
+    
 }
 
